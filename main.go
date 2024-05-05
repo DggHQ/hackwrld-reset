ac5bb60ffdf4d58a81b762bf83aa8e348f3b9dcf
Kevin Christen <kevin.christen@grabx.ch>
2024-05-05 22:35:39 +0200

    update mesasge

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func main() {
 			}
 			log.Println("Timer done. Deleting current game")
 			// Let players know game restarts
-			reminder := "[HACKWRLD will now start a new game. Please login here if you wanna play: https://hackwrld.notacult.website/]"
+			reminder := "[HACKWRLD will now start a new game in a few minutes. Please login here if you wanna play: https://hackwrld.notacult.website/]"
 			msg := Msg{
 				Data: reminder,
 			}
